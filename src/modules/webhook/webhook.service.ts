df25a76238428094117b97473b75cb0e2f375f01
Genny <genny@Gennys-MacBook-Pro.local>
2024-07-16 23:29:14 +0700

    fix_event_time_fix

--- src/modules/webhook/webhook.service.ts
+++ src/modules/webhook/webhook.service.ts
@@ -1,7 +1,8 @@
-import { Headers, Injectable, Req } from '@nestjs/common';
+import { BadRequestException, Headers, Injectable, Req } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as crypto from 'crypto';
 import { DiscordService } from '../discord/discord.service';
+import { BuildLogDTO } from '../discord/dto';
 @Injectable()
 export class WebHookService {
   secret = '';
@@ -26,52 +27,62 @@ export class WebHookService {
 
   public async githubHook(@Req() req: Request, @Headers() headers: any) {
     const payload: any = req.body;
+    // const secret = process.env.WEBHOOK_SECRET // Your webhook secret here
+    const signature = headers['x-hub-signature-256'];
 
-    return { message: payload };
+    if (!this.verifySignature(payload, this.secret, signature)) {
+      console.error('Invalid signature');
+      throw new BadRequestException('Invalid signature');
+    }
+    let event = '';
+    // Check if the webhook event is one of the events we're interested in
+    // const allowedEvents = ['deployment_status', 'workflow_job']; //, 'pull_request', 'push'] //,'workflow_run', 'workflow_job']
+    // if (!allowedEvents.includes(headers['x-github-event'] as string)) {
+    //   // console.log(`Unsupported event type: ${headers['x-github-event']}`)
+    //   return { success: false };
+    // }
+    event = headers['x-github-event'] as string;
+
     //#region xử lý data
 
-    // try {
-    //   switch (event) {
-    //     case 'workflow_job':
-    //       {
-    //         const deloyStatus = payload.deployment_status.state;
-    //         const creator = payload.sender.login;
-    //         // const description = payload.deployment_status.description;
-    //         const wfdisplay_title = payload.workflow_run.display_title;
-    //         const wfname = payload.workflow_run.name;
-    //         const repository = payload.repository.name;
-    //         const branch = payload.workflow_run.head_branch;
-    //         let stringNoti = `Dự án: ${repository}
-    //           Nhân viên: ${creator}
-    //           Nhánh: ${branch}
-    //           Commit: ${wfdisplay_title}`;
+    switch (event) {
+      case 'workflow_job':
+        {
+          const deloyStatus = payload.deployment_status.state;
+          const creator = payload.sender.login;
+          // const description = payload.deployment_status.description;
+          const wfdisplay_title = payload.workflow_run.display_title;
+          const wfname = payload.workflow_run.name;
+          const repository = payload.repository.name;
+          const branch = payload.workflow_run.head_branch;
+          let stringNoti = `Dự án: ${repository}
+              Nhân viên: ${creator}
+              Nhánh: ${branch}
+              Commit: ${wfdisplay_title}`;
 
-    //         const stringStatus = `Tiến trình ${wfname} - ${deloyStatus}`;
-    //         stringNoti = `${stringNoti} - ${stringStatus}`;
+          const stringStatus = `Tiến trình ${wfname} - ${deloyStatus}`;
+          stringNoti = `${stringNoti} - ${stringStatus}`;
 
-    //         const buildLogDTO = new BuildLogDTO();
-    //         buildLogDTO.branch = branch;
-    //         buildLogDTO.link = payload.deployment_status.target_url;
-    //         buildLogDTO.message = stringNoti;
-    //         buildLogDTO.project = repository;
-    //         buildLogDTO.source = 'Github';
-    //         buildLogDTO.statusName = stringStatus;
-    //         buildLogDTO.employeeName = creator;
+          const buildLogDTO = new BuildLogDTO();
+          buildLogDTO.branch = branch;
+          buildLogDTO.link = payload.deployment_status.target_url;
+          buildLogDTO.message = stringNoti;
+          buildLogDTO.project = repository;
+          buildLogDTO.source = 'Github';
+          buildLogDTO.statusName = stringStatus;
+          buildLogDTO.employeeName = creator;
 
-    //         this.discordService.buildLog(buildLogDTO);
-    //       }
-    //       break;
-    //     default:
-    //       break;
-    //   }
-    // } catch (error) {
-    //   return error;
-    // }
+          this.discordService.buildLog(buildLogDTO);
+        }
+        break;
+      default:
+        break;
+    }
 
-    // //#endregion
+    //#endregion
 
-    // // Handle the webhook payload here
-    // // console.log(`Received webhook payload: ${JSON.stringify(payload)}`)
-    // return { success: 'Build log success' };
+    // Handle the webhook payload here
+    // console.log(`Received webhook payload: ${JSON.stringify(payload)}`)
+    return { success: 'Build log success' };
   }
 }
