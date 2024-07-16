c69b991c7e3cdfdb5118d65ad50814fcea9096e1
Genny <genny@Gennys-MacBook-Pro.local>
2024-07-16 23:17:56 +0700

    fix_event_time

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ import { coreHelper } from './helpers';
 export class AppService {
   /** check status of server */
   healthCheck() {
-    return 'This server is healthy. - update v1 at 23:13PM deploy CI/CD';
+    return 'This server is healthy. - update v1 at 23:127PM deploy CI/CD';
   }
 
   /** check time by timezone of server */
